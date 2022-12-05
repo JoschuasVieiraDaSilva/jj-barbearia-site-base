37d09c4c1cec3892db99e7f5b3245e411a75cc6b
JosephdeM <73973117+JosephdeM@users.noreply.github.com>
2022-12-05 01:46:21 -0300

    Update login.php

--- login.php
+++ login.php
@@ -8,13 +8,13 @@
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="assets/css/style.css">
         <title>Formulário de Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!--<link rel="stylesheet" href="assets/css/bootstrap.min.css">-->
         <script src="assets/js/jquery-3.6.0.min.js"></script>
         <script src="assets/js/popper.min.js"></script>
         <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="assets/css/style.css">
     </head>
     <body>
         <div id="whatsapp_link">
@@ -25,27 +25,30 @@
         <div id="logo_size">
             <img src="assets/img/logo.png" class="logo">
         </div>
-        <div id="c" align="center"> <!--Conteudo principal-->
-            <div class="container m-3" >
-                <h3>Autenticação do Usuário</h3>
-                <form id="frmLogin" action="" method="POST">
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="email" class="form-control" id="email" placeholder="Digite seu email:" name="email">
-                    </div>
-                    <br/>
-                    <div class="form-group">
-                        <label for="pwd" id="labelpwd">Senha:</label>
-                        <input type="password" class="form-control" id="pwd" placeholder="Digite sua senha:" name="senha">
-                    </div>
-                    <br/>
-                    <button type="submit" id="btnLogin"  class="btn btn-primary">Enviar</button>
-                </form>
-                <p>Não possui uma conta?</p>
-                <a href="register.php" id="Register_link">Registre-se</a>
-                <div id="msg"></div>
+        <div id="formplaceholderI">
+            <div class="c" align="center"> <!--Conteudo principal-->
+                <div class="container m-3" >
+                    <h3>Autenticação do Usuário</h3>
+                    <form id="frmLogin" action="" method="POST">
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="email" class="form-control" id="email" placeholder="Digite seu email:" name="email">
+                        </div>
+                        <br/>
+                        <div class="form-group">
+                            <label for="pwd" id="labelpwd">Senha:</label>
+                            <input type="password" class="form-control" id="pwd" placeholder="Digite sua senha:" name="senha">
+                        </div>
+                        <br/>
+                        <button type="submit" id="btnLogin"  class="btn btn-primary">Enviar</button>
+                    </form>
+                    <p class="darktext">Não possui uma conta?</p>
+                    <a href="register.php" id="Register_link">Registre-se</a>
+                    <div id="msg"></div>
+                </div>
             </div>
         </div>
+        
         <div id="anterodape" class="spaceplus">
             <div class="subanterodape">
                 <h3>
